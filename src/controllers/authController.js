9657f8172d591e142480f2204bb0043609e60857
MahmoudAbdelsamie <msomaa38@gmail.com>
2024-12-19 14:58:50 +0200

    Edit Server Error Response For Register & Login Endpoint

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -32,7 +32,10 @@ const registerController = async (req, res) => {
         
         res.status(201).json({ message: 'User registered successfully', token });
     } catch (err) {
-        res.status(500).json({ error: 'Something went wrong', details: err.message });
+        res.status(500).json({
+            message: 'Internal Server Error!',
+            error: err.message 
+        });
     }
 };
 
@@ -57,7 +60,10 @@ const loginController = async (req, res) => {
         const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
         res.json({ message: 'Login successful', token });
     } catch (err) {
-        res.status(500).json({ error: 'Something went wrong', details: err.message });
+        res.status(500).json({
+            message: 'Internal Server Error!',
+            error: err.message 
+        });
     }
 };
 
