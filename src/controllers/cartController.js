5b39cd3b34428800eeedccdd11d36c26352d78da
MahmoudAbdelsamie <msomaa38@gmail.com>
2024-12-23 16:52:02 +0200

    Add add-item-to-cart endpoint

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -4,7 +4,7 @@ const prisma = require('../config/database');
 
 // Create a new cart --> POST /carts
 const createCart = async (req, res) => {
-    const { groupId, userId } = req.body; 
+    const { groupId, userId } = req.body;
     try {
         const group = await prisma.group.findUnique({ where: { id: groupId } });
         if (!group) {
@@ -33,10 +33,59 @@ const createCart = async (req, res) => {
 };
 
 
+// Add Item to Cart --> patch('/carts/:id/add-item')
+const addItemToCart = async (req, res) => {
+    const { id } = req.params;
+    const { productId, quantity } = req.body;
+    try {
+        const cart = await prisma.cart.findUnique({ where: { id: parseInt(id) } });
+        if(!cart) {
+            return res.status(404).json({ error: 'Cart Not Found!'})
+        }
+        const product = await prisma.product.findUnique({
+            where: {
+                id: productId
+            }
+        })
+        if(!product) {
+            return res.status(404).json({
+                error: 'product not found!'
+            })
+        }
+        const cartItem = await prisma.cartItem.upsert({
+            where: {
+                cartId_productId: {
+                    cartId: parseInt(id),
+                    productId,
+                },
+            },
+            update: {
+                quantity: { increment: quantity },
+            },
+            create: {
+                cartId: parseInt(id),
+                productId,
+                quantity,
+            },
+        });
+        res.json({
+            message: 'Item Added to cart',
+            cartItem
+        })
+    } catch(err) {
+        return res.status(500).json({
+            message: 'Internal Server Error',
+            error: err.message
+        })
+    }
+}
+
+
 
 
 
 
 module.exports = {
-    createCart
+    createCart,
+    addItemToCart
 }
--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const { createCart } = require('../controllers/cartController');
+const { createCart, addItemToCart } = require('../controllers/cartController');
 
 
 const router = express.Router();
 
 
-router.route('/carts').post(createCart)
+router.route('/carts').post(createCart);
+
+router
+    .route('/carts/:id/add-item')
+    .patch(addItemToCart)
 
 
 
